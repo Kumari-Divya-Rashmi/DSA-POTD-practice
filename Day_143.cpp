6e37636b5d00d3ccbd35578106503dcf28ce2e14
Kumari Divya Rashmi <kumaridivyarashmi306@gmail.com>
2025-07-24 16:59:49 +0530

    Day 143

---
+++ Day_143.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+#include <unordered_map>
+#include <stack>
+using namespace std;
+class Solution {
+  public:
+    int subarraySum(vector<int>& arr) {
+        // code here
+        int n = arr.size();
+        int result = 0;
+        for(int i = 0; i < n; i++){
+            result += arr[i] * (i + 1) * (n - i);
+        }
+        return result;
+    }
+};
